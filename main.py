4d0fccb0cfc8a35c3bf6c1f63574bd708f484856
dakhnod <dakhnod@gmail.com>
2023-04-16 00:48:58 +0200

    added reconnection logic

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import argparse
 import asyncio
 import pynput
 import dataclasses
+import sys
 
 class AIOKeyboardAdapter:
     @dataclasses.dataclass
@@ -46,54 +47,83 @@ class AIOKeyboardAdapter:
         return self.Binding(key, result.get('auto_release', auto_release_default))
 
     async def connect(self):
-        scanner = bleak.BleakScanner()
-        print('scanning...')
-        device = await scanner.find_device_by_name(self.config['name'], 60)
-        print('connecting...')
+        name = self.config.get('name')
+        address = self.config.get('address')
+        is_mac = sys.platform == 'Darwin'
 
-        self.client = bleak.BleakClient(device)
-        await self.client.connect()
+        disconnect_event = asyncio.Event()
 
-        print('connected.')
+        if is_mac and name is None:
+            raise RuntimeError('can only connect by device name on mac')
+        
+        async def client_connect(client):
+            while True:
+                try:
+                    print('connecting...', end='')
+                    await client.connect()
+                    print('success')
+                    break
+                except:
+                    print('failed')
+                    await asyncio.sleep(0.1)
+        
+        def on_disconnect(client):
+            print('client disconnected :(')
+            disconnect_event.set()
+        
+        if address is not None and not is_mac:
+            self.client = bleak.BleakClient(address)
+        elif name is not None:
+            print('scanning...')
+            scanner = bleak.BleakScanner()
+            device = await scanner.find_device_by_name(self.config['name'], 60)
+            self.client = bleak.BleakClient(device)
 
-        io_service = self.client.services['00001815-0000-1000-8000-00805f9b34fb']
-        for characteristic in io_service.characteristics:
-            if characteristic.uuid == '00002a56-0000-1000-8000-00805f9b34fb' and 'notify' in characteristic.properties:
-                input_characteristic = characteristic
-                break
-        else:
-            raise RuntimeError('Input IO characteristic not found')
-        def handle_input(characteristic, data):
-            print(characteristic, data)
-            for byte_index in range(len(data)):
-                for bit_index in range(0, 8, 2):
-                    bits = (data[byte_index] >> bit_index) & 0b11
-                    if bits == 0b11:
-                        continue
-                    is_pressed = bits == 0b01
-                    pin_index = byte_index * 4 + int(bit_index / 2)
-                    print(f'Pin {pin_index} is pressed: {is_pressed}')
-                    binding = self.get_binding_by_sensor_index(pin_index, is_pressed)
-                    binding_pressed = self.get_binding_by_sensor_index(pin_index, True)
-                    if is_pressed:
-                        if binding is None:
-                            print('no binding specified for pin')
-                            return
-                        self.keyboard.press(binding.key)
-                        if binding.auto_release:
-                            self.keyboard.release(binding.key)
-                    else:
-                        if not binding_pressed.auto_release:
-                            if binding is not None:
-                                raise RuntimeError('Cannot specify press.auto_release=False and release binding at the same time')
-                            self.keyboard.release(binding_pressed.key)
-                            return
-                        self.keyboard.tap(binding.key)
-                            
-        await self.client.start_notify(input_characteristic, handle_input)
-        print('waiting for notifications')
-        await asyncio.Future()
-        pass
+        while True:
+            await client_connect(self.client)
+
+            io_service = self.client.services['00001815-0000-1000-8000-00805f9b34fb']
+            for characteristic in io_service.characteristics:
+                if characteristic.uuid == '00002a56-0000-1000-8000-00805f9b34fb' and 'notify' in characteristic.properties:
+                    input_characteristic = characteristic
+                    break
+            else:
+                raise RuntimeError('Input IO characteristic not found')
+            def handle_input(characteristic, data):
+                print(characteristic, data)
+                for byte_index in range(len(data)):
+                    for bit_index in range(0, 8, 2):
+                        bits = (data[byte_index] >> bit_index) & 0b11
+                        if bits == 0b11:
+                            continue
+                        is_pressed = bits == 0b01
+                        pin_index = byte_index * 4 + int(bit_index / 2)
+                        print(f'Pin {pin_index} is pressed: {is_pressed}')
+                        binding = self.get_binding_by_sensor_index(pin_index, is_pressed)
+                        binding_pressed = self.get_binding_by_sensor_index(pin_index, True)
+                        if is_pressed:
+                            if binding is None:
+                                print('no binding specified for pin')
+                                return
+                            print(f'pressing key {binding.key}')
+                            self.keyboard.press(binding.key)
+                            if binding.auto_release:
+                                print(f'releasing key {binding.key}')
+                                self.keyboard.release(binding.key)
+                        else:
+                            if binding_pressed is not None and not binding_pressed.auto_release:
+                                if binding is not None:
+                                    raise RuntimeError('Cannot specify press.auto_release=False and release binding at the same time')
+                                
+                                print(f'releasing key {binding.key}')
+                                self.keyboard.release(binding_pressed.key)
+                                return
+                            print(f'taping key {binding.key}')
+                            self.keyboard.tap(binding.key)
+                                
+            await self.client.start_notify(input_characteristic, handle_input)
+            print('waiting for notifications')
+            await disconnect_event
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
